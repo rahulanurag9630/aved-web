94c3504f7e42d7ff941df2a6ea8d4dcf1d52ecb4
Anurag singh kushwaha <rahulanurag@gmail.com>
2025-07-29 00:25:43 +0530

    added live url

--- src/api-services/index.jsx
+++ src/api-services/index.jsx
@@ -2,7 +2,9 @@
 // const url = "https://nodepune-investment.mobiloitte.io/api/v1"
 // const url = "http://172.16.6.114:3044/api/v1";
 // export const url = "http://localhost:2211/api/v1"
-export const url = "https://aved-node.onrender.com/api/v1"
+// export const url = "https://aved-node.onrender.com/api/v1"  
+export const url = "http://3.20.115.19:2211/api/v1" // live
+
 
 
 export const socketNotification = "";
